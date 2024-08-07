e12ad6af2c1967f58356cc2b4ef988ae967f7c1b
Shreeya <shreeyacofficial@gmail.com>
2024-08-07 15:19:10 +0530

    Added

--- week1/q2/q2.py
+++ week1/q2/q2.py
@@ -2,26 +2,46 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def PCA(init_array: pd.DataFrame):
+def PCA(init_array: pd.DataFrame, dimensions: int = 2):
+    """ Perform PCA on the init_array and reduce it to the specified number of dimensions """
+    
+    # Standardize the data (zero mean)
+    standardized_data = (init_array - init_array.mean()) / init_array.std()
 
-    sorted_eigenvalues = None
-    final_data = None
-    dimensions = 2
+    # Compute the covariance matrix
+    covariance_matrix = np.cov(standardized_data, rowvar=False)
 
-    # TODO: transform init_array to final_data using PCA
+    # Compute the eigenvalues and eigenvectors
+    eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
 
-    # END TODO
+    # Sort the eigenvalues and eigenvectors in descending order
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[sorted_indices]
+    sorted_eigenvectors = eigenvectors[:, sorted_indices]
+
+    # Select the top 'dimensions' eigenvectors
+    selected_eigenvectors = sorted_eigenvectors[:, :dimensions]
+
+    # Transform the data
+    final_data = np.dot(standardized_data, selected_eigenvectors)
 
     return sorted_eigenvalues, final_data
 
-
 if __name__ == '__main__':
-    init_array = pd.read_csv("pca_data.csv", header = None)
+    init_array = pd.read_csv("pca_data.csv", header=None)
     sorted_eigenvalues, final_data = PCA(init_array)
-    np.savetxt("transform.csv", final_data, delimiter = ',')
+
+    # Save the transformed data
+    np.savetxt("transform.csv", final_data, delimiter=',')
+
+    # Print the sorted eigenvalues
     for eig in sorted_eigenvalues:
         print(eig)
 
-    # TODO: plot and save a scatter plot of final_data to out.png
-
-    # END TODO
+    # Plot and save a scatter plot of the final_data
+    plt.scatter(final_data[:, 0], final_data[:, 1])
+    plt.title("PCA Scatter Plot")
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.savefig("out.png")
+    plt.show()
